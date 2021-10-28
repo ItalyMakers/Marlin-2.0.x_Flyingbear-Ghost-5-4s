b0cf476746e11b43bade3c5721e14ee97b8e4a43
Davide Zavattero <davide@davidezavattero.com>
2021-10-28 20:58:35 +0200

    aggiunta preheating

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1387,10 +1387,10 @@
 /**
  * Auto-leveling needs preheating
  */
-// #define PREHEAT_BEFORE_LEVELING
+#define PREHEAT_BEFORE_LEVELING
 #if ENABLED(PREHEAT_BEFORE_LEVELING)
   #define LEVELING_NOZZLE_TEMP 200   // (°C) Only applies to E0 at this time
-  #define LEVELING_BED_TEMP     60
+  #define LEVELING_BED_TEMP     55
 #endif
 
 /**
