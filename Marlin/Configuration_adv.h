689064fed76b5e53704907fd1a0ed18b96615f0b
Davide Zavattero <davide@davidezavattero.com>
2021-12-19 19:26:55 +0100

    fix for build

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1492,7 +1492,7 @@
    *
    * [1] On AVR an interrupt-capable pin is best for UHS3 compatibility.
    */
-  #define USB_FLASH_DRIVE_SUPPORT
+  // #define USB_FLASH_DRIVE_SUPPORT
   #if ENABLED(USB_FLASH_DRIVE_SUPPORT)
     /**
      * USB Host Shield Library
@@ -1552,7 +1552,7 @@
   //#define NO_SD_DETECT
 
   // Multiple volume support - EXPERIMENTAL.
-  #define MULTI_VOLUME
+  // #define MULTI_VOLUME
   #if ENABLED(MULTI_VOLUME)
     #define VOLUME_SD_ONBOARD       1
     #define VOLUME_USB_FLASH_DRIVE  2
